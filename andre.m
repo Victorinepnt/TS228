709e675e3498dadd5a0e801ba2eb5f0b0540fe0e
Victorine Penot <Victorine.Penot@bordeaux-inp.fr>
2023-01-30 18:08:55 +0100

    oidjheaif

--- andre.m
+++ andre.m
@@ -2,3 +2,10 @@ clear;
 close all;
 clc;
 
+tic;
+%addpath('hw3data');
+%addpath('hw3data/training set');
+[trainImgs,trainLabels] = readMNIST('train-images-idx3-ubyte','train-labels-idx1-ubyte',20000,0);
+%addpath('hw3data/test set');
+[testImgs,testLabels] = readMNIST('t10k-images-idx3-ubyte','t10k-labels-idx1-ubyte',10000,0);
+toc;
